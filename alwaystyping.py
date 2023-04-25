72972036ba592aca8d315028ebf4b5d978efffd4
thatnameistaken <95547419+thatnameistaken@users.noreply.github.com>
2023-04-25 18:59:06 +1000

    Update alwaystyping.py

--- alwaystyping.py
+++ alwaystyping.py
@@ -1,11 +1,20 @@
 import pyautogui
 import time
+# wait 5 seconds
 time.sleep(5)
+# type 2
 pyautogui.typewrite('2')
+# loop start
 while True:
+    # wait 1 second
     time.sleep(1)
+    # type 1
     pyautogui.typewrite('1')
+    # wait 1 second
     time.sleep(1)
+    # press backspace
     pyautogui.press('backspace')
+    # wait 1 second
     time.sleep(1)
+    
     
